4db31b312254ac22b5ad9c14ec0296d01462b7bb
marksterben <68014273+marksterben@users.noreply.github.com>
2021-09-23 17:20:53 +0700

    Create index.blade.php

---
+++ kantong-medis/resources/views/admin/pages/partner/index.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Admin</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Username</th>
+                            <th>Nama</th>
+                            <th>Alamat</th>
+                            <th>Telepon</th>
+                            <th>Link Map</th>
+                            <th>Gambar</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($partners as $row)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $row->username }}</td>
+                                <td>{{ $row->name }}</td>
+                                <td>{{ $row->address }}</td>
+                                <td>{{ $row->phone }}</td>
+                                <td>{{ $row->map }}</td>
+                                <td>
+                                    <img src="{{ asset("storage/$row->image") }}" alt="error!" style="max-width: 200px;">
+                                </td>
+                                <td>{{ $row->status }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
