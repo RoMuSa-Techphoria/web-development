31366caf8fca04d8d31ce8e7d56f844690ca0c44
GueRefaldy <refaldy.bagas10@gmail.com>
2021-09-23 17:41:08 +0700

    Update admin.php

--- kantong-medis/routes/admin.php
+++ kantong-medis/routes/admin.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('partner', PartnerController::class);
+Route::resource('category', CategoryController::class);
+
