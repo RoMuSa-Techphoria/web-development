909653865c81051a81665ff4eddb1c9ccaeb088f
GueRefaldy <refaldy.bagas10@gmail.com>
2021-09-24 16:23:47 +0700

    category done

--- kantong-medis/resources/views/admin/layouts/app.blade.php
+++ kantong-medis/resources/views/admin/layouts/app.blade.php
@@ -50,7 +50,7 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{route('category.index')}}">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Category</span></a>
             </li>
