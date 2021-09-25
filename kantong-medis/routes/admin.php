393baea83afba3cc1e394404a2b1b95245348cac
Thingkilia Finnatia Husin <vikasatibrahma@gmail.com>
2021-09-25 11:41:38 +0700

    login kemarin

--- kantong-medis/routes/admin.php
+++ kantong-medis/routes/admin.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/panel/admin/login', [AuthController::class, "formAdmin"]);
+Route::get('/panel/login', [AuthController::class, "formPartner"]);
 Route::resource('partner', PartnerController::class);
 Route::resource('category', CategoryController::class);
 
