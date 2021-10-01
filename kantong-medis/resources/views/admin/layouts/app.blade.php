e854f2dceb6b253e779a521ddd92561b98097108
GueRefaldy <refaldy.bagas10@gmail.com>
2021-10-01 12:20:49 +0700

    service

--- kantong-medis/resources/views/admin/layouts/app.blade.php
+++ kantong-medis/resources/views/admin/layouts/app.blade.php
@@ -50,14 +50,14 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="{{route('category.index')}}">
+                <a class="nav-link" href="{{ route('category.index') }}">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Category</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('service.index') }}">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Service</span></a>
             </li>
